2f2322e03852c009257ed76f637d26ce7bed1eb6
Mike Ammerlaan <mike@bendyline.com>
2026-01-16 21:29:26 -0800

    fix: Update

--- Readme.md
+++
@@ -1,3 +0,0 @@
-## About Qualla
-
-This is the hub for information about Qualla.
--- updatecore.cmd
+++
@@ -1,2 +0,0 @@
-copy ..\facer\build\facer\js\facersite.css lib\facer\
-copy ..\facer\build\facer\js\facersite.js lib\facer\
